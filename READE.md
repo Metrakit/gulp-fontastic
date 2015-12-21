2174d57c1daed0eb64b01a6692c2e6257e318a01
Jordane Jouffroy <contact@jordane.net>
2015-12-21 22:24:30 +0100

    first commit

---
+++ READE.md
@@ -0,0 +1,25 @@
+# Download your fonts from the awesome Fontastic webapp
+
+This Gulp plugin will download your fonts from Fontastic webapp (https://app.fontastic.me) and copy in folder the fonts with a hash and the style in .css file or .scss file.
+
+I have make this plugin because for my personnal developments I'm using the Fontastic CDN when I'm developing then I'm using the fonts in local for the production. So this plugin can get automatically the fonts thanks to your gulp workflow.
+
+## Usage :
+
+```
+var fontastic      = require('gulp-fontastic');
+
+gulp.task('fontastic', function() {
+    fontastic({
+        key: 'your-api-key',
+        font_path: 'fonts/',
+        style_path: 'scss/',
+        scss: true
+    });
+});
+```
+
+## TODO
+
+- Create fonts and style folders if they doesnt exist
+- Clean the fonts folder before to generate the new fonts
---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var fs = require('fs');
+var request = require('request');
+
+var PLUGIN_NAME = 'gulp-fontastic';
+
+module.exports = function(opt) {
+  request('https://file.myfontastic.com/' + opt.key + '/icons.css', function (error, response, body) {
+    var content = response.body;
+    var results = content.match(/([0-9]+).eot/);
+    var hash = results[1];
+
+    var url = 'https://file.myfontastic.com/'+ opt.key + '/fonts/' + hash;
+
+    request(url + '.eot').pipe(fs.createWriteStream(opt.font_path + hash + '.eot'));
+    request(url + '.woff').pipe(fs.createWriteStream(opt.font_path + hash + '.woff'));
+    request(url + '.ttf').pipe(fs.createWriteStream(opt.font_path + hash + '.ttf'));
+    request(url + '.svg').pipe(fs.createWriteStream(opt.font_path + hash + '.svg'));
+
+    var re = new RegExp(url, 'g');
+    content = content.replace(re, opt.font_path + hash);
+
+    var extension = "css";
+    if (opt.scss === true) {
+      extension = "scss";
+    }
+
+    fs.writeFile(opt.style_path + 'font.' + extension, content);
+
+  });
+};
