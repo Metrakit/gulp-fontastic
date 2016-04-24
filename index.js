b691edf1c1803ce7f90abae2de1c9f7137e09b87
Jordane Jouffroy <contact@jordane.net>
2016-04-24 10:51:57 +0200

    add stream system

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var request = require('request');
 var _ = require('underscore');
+var transform   = require('stream').Transform;
 
 var PLUGIN_NAME = 'gulp-fontastic';
 
@@ -17,34 +18,39 @@ var options = {
 
 module.exports = function(opt) {
 
-    options = _.extend(options, opt);
-    request('https://file.myfontastic.com/' + options.key + '/icons.css', function (error, response, body) {
+    var stream = new transform({objectMode: true});
 
-        var content = response.body;
-        var results = content.match(/([0-9]+).eot/);
-        var hash = results[1];
+    stream._transform = function (file, encoding, cb) {
+        options = _.extend(options, opt);
+        request('https://file.myfontastic.com/' + options.key + '/icons.css', function (error, response, body) {
 
-        var url = 'https://file.myfontastic.com/'+ options.key + '/fonts/' + hash;
+            var content = response.body;
+            var results = content.match(/([0-9]+).eot/);
+            var hash = results[1];
 
-        var font_name = hash;
-        if (options.font_name) {
-            font_name = options.font_name;
-        }
+            var url = 'https://file.myfontastic.com/'+ options.key + '/fonts/' + hash;
 
-        request(url + '.eot').pipe(fs.createWriteStream(options.font_dir + font_name + '.eot'));
-        request(url + '.woff').pipe(fs.createWriteStream(options.font_dir + font_name + '.woff'));
-        request(url + '.ttf').pipe(fs.createWriteStream(options.font_dir + font_name + '.ttf'));
-        request(url + '.svg').pipe(fs.createWriteStream(options.font_dir + font_name + '.svg'));
+            var font_name = hash;
+            if (options.font_name) {
+                font_name = options.font_name;
+            }
 
-        var re = new RegExp(url, 'g');
-        content = content.replace(re, options.font_path + font_name);
+            request(url + '.eot').pipe(fs.createWriteStream(options.font_dir + font_name + '.eot'));
+            request(url + '.woff').pipe(fs.createWriteStream(options.font_dir + font_name + '.woff'));
+            request(url + '.ttf').pipe(fs.createWriteStream(options.font_dir + font_name + '.ttf'));
+            request(url + '.svg').pipe(fs.createWriteStream(options.font_dir + font_name + '.svg'));
 
-        var extension = "css";
-        if (options.scss === true) {
-            extension = "scss";
-        }
+            var re = new RegExp(url, 'g');
+            content = content.replace(re, options.font_path + font_name);
 
-        fs.writeFile(options.style_path + options.file_name + '.' + extension, content);
+            var extension = "css";
+            if (options.scss === true) {
+                extension = "scss";
+            }
 
-    });
+            fs.writeFile(options.style_path + options.file_name + '.' + extension, content);
+            cb();
+        });
+    };
+    return stream;
 };
